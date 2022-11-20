7990ea580a85d419b8c95eb7c19c2e74255da6c8
MaciejKarnowski <101185253+MaciejKarnowski@users.noreply.github.com>
2022-11-20 19:33:25 +0100

    optimalization
    added: change text to lower case
    optimalization.

--- venv/Scripts/Count_words.py
+++ venv/Scripts/Count_words.py
@@ -1,19 +1,13 @@
 from keras.preprocessing.text import Tokenizer
 from operator import itemgetter
 
-def text_to_list_words(text):
-    separators = ['\n', '`', '*', '_', '{', '}', '[', ']', '(', ')', '>', '#', '+', '-', '.', '!', '$', ',']
-    for sep in separators:
-        text = text.replace(sep, " ")
-    text = text.split(" ")
-    text = list(filter(None, text))
-    return text
+separators = ['\n', '`', '*', '_', '{', '}', '[', ']', '(', ')', '>', '#', '+', '-', '.', '!', '$', ',']
 
 
-def count_words(file, n): #file - filename, n - number of words to display
-    with open(file,encoding="utf-8") as f:
-        text = text_to_list_words(f.read())
-    t = Tokenizer()
+def count_words(file, n):  # file - filename, n - number of words to display
+    with open(file, encoding="utf-8") as f:
+        text = f.read()
+    t = Tokenizer(filters=separators,lower=True,split=" ")
     t.fit_on_texts(text)
     word_list = sorted(t.word_counts.items(), key=itemgetter(1), reverse=True)
     while word_list[n][1] == word_list[n + 1][1]:  # działa, sprawdzone poprzez usunięcie reverse=True
@@ -21,4 +15,4 @@ def count_words(file, n): #file - filename, n - number of words to display
     return print("Word Count:", word_list[:n])
 
 
-count_words("potop.txt",20)
+count_words("potop.txt", 20)
