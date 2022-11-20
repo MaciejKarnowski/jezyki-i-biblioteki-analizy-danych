d9af6b2e9f1cdbdc727755d1f94e17cde96fff22
MaciejKarnowski <101185253+MaciejKarnowski@users.noreply.github.com>
2022-11-20 18:06:37 +0100

    Update Count_words.py

--- venv/Scripts/Count_words.py
+++ venv/Scripts/Count_words.py
@@ -15,7 +15,10 @@ def count_words(file, n): #file - filename, n - number of words to display
         text = text_to_list_words(f.read())
     t = Tokenizer()
     t.fit_on_texts(text)
-    return print("Word Count:", sorted(t.word_counts.items(), key=itemgetter(1), reverse = True)[:n])
+    word_list = sorted(t.word_counts.items(), key=itemgetter(1), reverse=True)
+    while word_list[n][1] == word_list[n + 1][1]:  # działa, sprawdzone poprzez usunięcie reverse=True
+        n = n + 1
+    return print("Word Count:", word_list[:n])
 
 
 count_words("potop.txt",20)
