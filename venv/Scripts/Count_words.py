04b2b4daf3648475cdc9591d6dd25aaaf730ae99
MaciejKarnowski <101185253+MaciejKarnowski@users.noreply.github.com>
2022-11-20 17:56:24 +0100

    Update Count_words.py

--- venv/Scripts/Count_words.py
+++ venv/Scripts/Count_words.py
@@ -9,12 +9,13 @@ def text_to_list_words(text):
     text = list(filter(None, text))
     return text
 
-def count_words(file,n): #file - filename, n - number of words to display
+
+def count_words(file, n): #file - filename, n - number of words to display
     with open(file,encoding="utf-8") as f:
         text = text_to_list_words(f.read())
-        
     t = Tokenizer()
     t.fit_on_texts(text)
     return print("Word Count:", sorted(t.word_counts.items(), key=itemgetter(1), reverse = True)[:n])
+
 
 count_words("potop.txt",20)
